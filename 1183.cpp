a88f6d83d399a1f41476b8f5435509e40f4a27d1
Md Abul Kalam <ojkalam@gmail.com>
2017-06-03 14:42:47 +0600

    Create 1183.cpp

---
+++ 1183.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int i,j,c=0;
+    float sum=0.0,a;
+    char ch;
+    cin>>ch;
+    for(i=0;i<12;i++){
+
+        for(j=0;j<12;j++){
+            cin>>a;
+            if(j>c){
+                sum=sum+a;
+            }
+        }
+      c=c+1;
+    }
+    if(ch=='S'){
+        printf("%0.1f\n",sum);
+    }
+    else{
+        printf("%0.1f\n",sum/66);
+    }
+    return 0;
+}
