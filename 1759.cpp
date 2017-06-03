038c40fc72a95fe059aa68f0ec69f2dfd7466d31
Md Abul Kalam <ojkalam@gmail.com>
2017-06-03 14:36:24 +0600

    Create 1759.cpp

---
+++ 1759.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int n,i;
+    cin>>n;
+    for(i=1;i<=n;i++){
+         if(i<n){
+            cout<<"Ho ";
+         }
+         else
+            cout<<"Ho";
+
+    }
+    cout<<"!"<<endl;
+    return 0;
+}
