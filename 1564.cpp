8be4a946884a297f14ab787fad2b42c306c54c9d
Md Abul Kalam <ojkalam@gmail.com>
2017-06-03 14:37:39 +0600

    Create 1564.cpp

---
+++ 1564.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int n;
+    while (scanf("%d",&n) !=EOF){
+        if(n>0){
+            cout<<"vai ter duas!"<<endl;
+        }
+        else{
+            cout<<"vai ter copa!"<<endl;
+        }
+    }
+    return 0;
+}
