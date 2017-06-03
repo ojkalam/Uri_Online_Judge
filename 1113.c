41112d5c9d86b82f81d1db1d1bbae245c87c898b
Md Abul Kalam <ojkalam@gmail.com>
2017-06-03 15:12:18 +0600

    Create 1113.c

---
+++ 1113.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,x,y;
+    for(i=1;i==1;){
+        scanf("%d%d",&x,&y);
+        if(x>y){
+            printf("Decrescente\n");
+        }
+        else if(x<y){
+            printf("Crescente\n");
+        }
+        else if(x==y){
+            break;
+        }
+
+    }
+    return 0;
+}
